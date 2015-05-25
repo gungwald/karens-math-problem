f0d1f45fc6d085cf241b30a814b2b5631f38b07d
Bill Chatfield <bill_chatfield@yahoo.com>
2015-05-25 17:21:07 -0400

    Create karens-math-problem.bat

---
+++ karens-math-problem.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem Runs the JavaScript program in the cscript interpreter.
+cscript //nologo karens-math-problem.js
+
+rem Wait for the user to press a key so the window doesn't disappear before
+rem you can see the results.
+pause
